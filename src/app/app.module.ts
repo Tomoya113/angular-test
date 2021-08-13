f040c480e2af35a20353773557c559a3fe7bc626
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-08-14 03:05:15 +0900

    HTTPClientModuleをapp.module.tsに追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuBarComponent,
-    PostsComponent,
-    PostComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MenuBarComponent, PostsComponent, PostComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
