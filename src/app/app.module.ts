a1c17d3caa8e3466544f1319d8c66e28ab3928c7
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-08-14 03:02:01 +0900

    Postsコンポーネントの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
